b9b1e8900651004d8774e569a57a055c8c279382
misterCh0c <wojhack@unseen.is>
2015-05-24 15:04:53 +0200

    Signed-off-by: misterCh0c <wojhack@unseen.is>

--- vue/admin/partie.php
+++ vue/admin/partie.php
@@ -3,10 +3,10 @@
 <style>
 //table, th, td {    border: 1px solid black;}
 </style>
-<table border='0' cellspacing='0' width='100%' class="NONE">
+<table border='0' id="seltb" cellspacing='0' width='100%' class="NONE">
     <tr>
         <td class='JH'>
-            <select class="form-control" id="sel1" onchange="checkDuplicate()">
+            <select class="form-control sel" id="sel1" onchange="checkDuplicate()">
                 <option value="player1">Joueur 1</option>
                 <?php
                 for ($i=0;$i<count($item);$i++){
@@ -15,7 +15,9 @@
                 echo '</option>';
                 };
                 ?>
+                
             </select>
+
         </div></td>
         <td width='25%'class='JH' rowspan=2><a name='b6'>&nbsp;</a></td>
         <td width='25%'class='JH' rowspan=4><a name='b2'>&nbsp;</a></td>
@@ -26,7 +28,7 @@
     </tr>
     <tr>
         <td class='JB'>
-            <select class="form-control" id="sel2" onchange="checkDuplicate()">
+            <select class="form-control sel" id="sel2" onchange="checkDuplicate()">
                 <option value="player2">Joueur 2</option>
                 <?php
                 for ($i=0;$i<count($item);$i++){
@@ -43,7 +45,18 @@
         <td class='JBS'><a name='b13'>&nbsp;</a></td>
     </tr>
     <tr>
-        <td class='JH'> <a name='b12'>Joueur 3</a></a></td>
+        <td class='JH'>
+            <select class="form-control sel" id="sel3" onchange="checkDuplicate()">
+                <option value="player3">Joueur 3</option>
+                <?php
+                for ($i=0;$i<count($item);$i++){
+                echo"<option value=' $i '>";
+                    echo $item[$i]->nom;
+                echo '</option>';
+                };
+                ?>
+            </select>
+        </div></td>
         <td class='JB' rowspan=2><a name='b5'>&nbsp;</a></td>
         <td class='JHS' rowspan=4><a name='b2'>&nbsp;</a></td>
     </tr>
@@ -51,14 +64,36 @@
         <td class='JHS'><a name='b12'>&nbsp;</a></td>
     </tr>
     <tr>
-        <td class='JB'> <a name='b11'>Joueur 4 </a></a></td>
+        <td class='JB'>
+            <select class="form-control sel" id="sel4" onchange="checkDuplicate()">
+                <option value="player4">Joueur 4</option>
+                <?php
+                for ($i=0;$i<count($item);$i++){
+                echo"<option value=' $i '>";
+                    echo $item[$i]->nom;
+                echo '</option>';
+                };
+                ?>
+            </select>
+        </div></td>
         <td class='JBS' rowspan=2><a name='b5'>&nbsp;</a></td>
     </tr>
     <tr>
         <td class='JBS'><a name='b11'>&nbsp;</a></td>
     </tr>
     <tr>
-        <td class='JH'> <a name='b10'>Joueur 5 </a></a></td>
+        <td class='JH'>
+            <select class="form-control sel" id="sel5" onchange="checkDuplicate()">
+                <option value="player5">Joueur 5</option>
+                <?php
+                for ($i=0;$i<count($item);$i++){
+                echo"<option value=' $i '>";
+                    echo $item[$i]->nom;
+                echo '</option>';
+                };
+                ?>
+            </select>
+        </div></td>
         <td class='JH' rowspan=2><a name='b4'>&nbsp;</a></td>
         <td class='JB' rowspan=4><a name='b1'>&nbsp;</a></td>
         <td class='JBS' rowspan=8><a name='b0'>&nbsp;</a></td>
@@ -66,15 +101,36 @@
     <tr>
         <td class='JHS'><a name='b10'>&nbsp;</a></td>
     </tr>
-    <tr>
-        <td class='JB'> <a name='b9'>Joueur 6</a></a></td>
+        <td class='JB'>
+            <select class="form-control sel" id="sel6" onchange="checkDuplicate()">
+                <option value="player6">Joueur 6</option>
+                <?php
+                for ($i=0;$i<count($item);$i++){
+                echo"<option value=' $i '>";
+                    echo $item[$i]->nom;
+                echo '</option>';
+                };
+                ?>
+            </select>
+        </div></td>
         <td class='JHS' rowspan=2><a name='b4'>&nbsp;</a></td>
     </tr>
     <tr>
         <td class='JBS'><a name='b9'>&nbsp;</a></td>
     </tr>
     <tr>
-        <td class='JH'> <a name='b8'>Joueur 7</a></td>
+        <td class='JH'>
+            <select class="form-control sel" id="sel7" onchange="checkDuplicate()">
+                <option value="player7">Joueur 7</option>
+                <?php
+                for ($i=0;$i<count($item);$i++){
+                echo"<option value=' $i '>";
+                    echo $item[$i]->nom;
+                echo '</option>';
+                };
+                ?>
+            </select>
+        </div></td>
         <td class='JB' rowspan=2><a name='b3'>&nbsp;</a></td>
         <td class='JBS' rowspan=4><a name='b1'>&nbsp;</a></td>
     </tr>
@@ -82,12 +138,27 @@
         <td class='JHS'><a name='b8'>&nbsp;</a></td>
     </tr>
     <tr>
-        <td class='JB'> <a name='b7'>Joueur 8</a></td>
+        <td class='JB'>
+            <select class="form-control sel" id="sel8" onchange="checkDuplicate()">
+                <option value="player8">Joueur 8</option>
+                <?php
+                for ($i=0;$i<count($item);$i++){
+                echo"<option value=' $i '>";
+                    echo $item[$i]->nom;
+                echo '</option>';
+                };
+                ?>
+            </select>
+        </div></td>
         <td class='JBS' rowspan=2><a name='b3'>&nbsp;</a></td>
     </tr>
     <tr>
         <td class='JBS'><a name='b7'>&nbsp;</a></td>
     </tr>
 </table>
-<button type="button" class="btn btn-success">Valider</button>
+<button type="button" id="validerbtn" class="btn btn-success" onclick="disableInputs()">Valider</button>
+
+
+
+
 <?php   require "$racine/vue/footer.php"; ?>
--- vue/js/partie.js
+++ vue/js/partie.js
@@ -2,9 +2,20 @@
 function checkDuplicate() {
     var selOne = document.getElementById("sel1").value;
     var selTwo = document.getElementById("sel2").value;
-    console.log(selOne);
-    console.log(selTwo);
+
     if (selOne == selTwo) { //|| selThree...
         alert("select are the same");
     }
+}
+
+function disableInputs(){
+//$("#validerbtn").on("click", function(){
+
+
+
+	$("select").each(function(e,r){
+		console.log(r.id);
+		$("#"+r.id).prop('disabled',true);
+	});
+
 }
