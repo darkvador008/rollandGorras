4971452a7f81fe295016ec2be7030b5c77d329ba
misterCh0c <wojhack@unseen.is>
2015-05-24 16:39:56 +0200

    disable select
    Signed-off-by: misterCh0c <wojhack@unseen.is>

--- vue/admin/partie.php
+++ vue/admin/partie.php
@@ -1,5 +1,4 @@
 <?php   require "$racine/vue/header.php"; ?>
-
 <style>
 //table, th, td {    border: 1px solid black;}
 </style>
@@ -15,150 +14,160 @@
                 echo '</option>';
                 };
                 ?>
-                
-            </select>
-
-        </div></td>
-        <td width='25%'class='JH' rowspan=2><a name='b6'>&nbsp;</a></td>
-        <td width='25%'class='JH' rowspan=4><a name='b2'>&nbsp;</a></td>
-        <td width='25%'class='JH' rowspan=8><a name='b0'>&nbsp;</a></td>
-    </tr>
-    <tr>
-        <td class='JHS'><a name='b14'>&nbsp;</a></td>
-    </tr>
-    <tr>
-        <td class='JB'>
-            <select class="form-control sel" id="sel2" onchange="checkDuplicate()">
-                <option value="player2">Joueur 2</option>
-                <?php
-                for ($i=0;$i<count($item);$i++){
-                echo"<option value=' $i '>";
-                    echo $item[$i]->nom;
-                echo '</option>';
-                };
-                ?>
             </select>
         </td>
-        <td class='JHS' rowspan=2><a name='b6'>&nbsp;</a></td>
-    </tr>
-    <tr>
-        <td class='JBS'><a name='b13'>&nbsp;</a></td>
-    </tr>
-    <tr>
-        <td class='JH'>
-            <select class="form-control sel" id="sel3" onchange="checkDuplicate()">
-                <option value="player3">Joueur 3</option>
-                <?php
-                for ($i=0;$i<count($item);$i++){
-                echo"<option value=' $i '>";
-                    echo $item[$i]->nom;
-                echo '</option>';
-                };
-                ?>
-            </select>
-        </div></td>
-        <td class='JB' rowspan=2><a name='b5'>&nbsp;</a></td>
-        <td class='JHS' rowspan=4><a name='b2'>&nbsp;</a></td>
-    </tr>
-    <tr>
-        <td class='JHS'><a name='b12'>&nbsp;</a></td>
-    </tr>
-    <tr>
-        <td class='JB'>
-            <select class="form-control sel" id="sel4" onchange="checkDuplicate()">
-                <option value="player4">Joueur 4</option>
-                <?php
-                for ($i=0;$i<count($item);$i++){
-                echo"<option value=' $i '>";
-                    echo $item[$i]->nom;
-                echo '</option>';
-                };
-                ?>
-            </select>
-        </div></td>
-        <td class='JBS' rowspan=2><a name='b5'>&nbsp;</a></td>
-    </tr>
-    <tr>
-        <td class='JBS'><a name='b11'>&nbsp;</a></td>
-    </tr>
-    <tr>
-        <td class='JH'>
-            <select class="form-control sel" id="sel5" onchange="checkDuplicate()">
-                <option value="player5">Joueur 5</option>
-                <?php
-                for ($i=0;$i<count($item);$i++){
-                echo"<option value=' $i '>";
-                    echo $item[$i]->nom;
-                echo '</option>';
-                };
-                ?>
-            </select>
-        </div></td>
-        <td class='JH' rowspan=2><a name='b4'>&nbsp;</a></td>
-        <td class='JB' rowspan=4><a name='b1'>&nbsp;</a></td>
-        <td class='JBS' rowspan=8><a name='b0'>&nbsp;</a></td>
-    </tr>
-    <tr>
-        <td class='JHS'><a name='b10'>&nbsp;</a></td>
-    </tr>
-        <td class='JB'>
-            <select class="form-control sel" id="sel6" onchange="checkDuplicate()">
-                <option value="player6">Joueur 6</option>
-                <?php
-                for ($i=0;$i<count($item);$i++){
-                echo"<option value=' $i '>";
-                    echo $item[$i]->nom;
-                echo '</option>';
-                };
-                ?>
-            </select>
-        </div></td>
-        <td class='JHS' rowspan=2><a name='b4'>&nbsp;</a></td>
-    </tr>
-    <tr>
-        <td class='JBS'><a name='b9'>&nbsp;</a></td>
-    </tr>
-    <tr>
-        <td class='JH'>
-            <select class="form-control sel" id="sel7" onchange="checkDuplicate()">
-                <option value="player7">Joueur 7</option>
-                <?php
-                for ($i=0;$i<count($item);$i++){
-                echo"<option value=' $i '>";
-                    echo $item[$i]->nom;
-                echo '</option>';
-                };
-                ?>
-            </select>
-        </div></td>
-        <td class='JB' rowspan=2><a name='b3'>&nbsp;</a></td>
-        <td class='JBS' rowspan=4><a name='b1'>&nbsp;</a></td>
-    </tr>
-    <tr>
-        <td class='JHS'><a name='b8'>&nbsp;</a></td>
-    </tr>
-    <tr>
-        <td class='JB'>
-            <select class="form-control sel" id="sel8" onchange="checkDuplicate()">
-                <option value="player8">Joueur 8</option>
-                <?php
-                for ($i=0;$i<count($item);$i++){
-                echo"<option value=' $i '>";
-                    echo $item[$i]->nom;
-                echo '</option>';
-                };
-                ?>
-            </select>
-        </div></td>
-        <td class='JBS' rowspan=2><a name='b3'>&nbsp;</a></td>
-    </tr>
-    <tr>
-        <td class='JBS'><a name='b7'>&nbsp;</a></td>
-    </tr>
+        
+        <td width='25%'class='JH' rowspan=2><a name='b6'>&nbsp;</a><button type="button" class="btn btn-default scorebtntop" aria-label="Left Align">
+        <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+        </button>
+        <button type="button" class="btn btn-default scorebtntop" aria-label="Left Align">
+        <span class="glyphicon glyphicon-minus" aria-hidden="true"></span>
+        </button></td>
+
+    <td width='25%'class='JH' rowspan=4><a name='b2'>&nbsp;</a></td>
+    <td width='25%'class='JH' rowspan=8><a name='b0'>&nbsp;</a></td>
+</tr>
+<tr>
+    <td class='JHS'><a name='b14'>&nbsp;</a></td>
+</tr>
+<tr>
+    <td class='JB'>
+        <select class="form-control sel" id="sel2" onchange="checkDuplicate()">
+            <option value="player2">Joueur 2</option>
+            <?php
+            for ($i=0;$i<count($item);$i++){
+            echo"<option value=' $i '>";
+                echo $item[$i]->nom;
+            echo '</option>';
+            };
+            ?>
+        </select>
+    </td>
+
+    <!-- SCORE BTN -->
+    <td class='JHS' rowspan=2><a name='b6'>&nbsp;</a><button type="button" class="btn btn-default scorebtn" aria-label="Left Align">
+        <div >
+        <span class="glyphicon glyphicon-plus " aria-hidden="true"></span>
+        </button>
+        <button type="button" class="btn btn-default scorebtn" aria-label="Left Align">
+        <span class="glyphicon glyphicon-minus" aria-hidden="true"></span>
+        </button></div></td>
+</td>
+</tr>
+<tr>
+    <td class='JBS'><a name='b13'>&nbsp;</a></td>
+</tr>
+<tr>
+    <td class='JH'>
+        <select class="form-control sel" id="sel3" onchange="checkDuplicate()">
+            <option value="player3">Joueur 3</option>
+            <?php
+            for ($i=0;$i<count($item);$i++){
+            echo"<option value=' $i '>";
+                echo $item[$i]->nom;
+            echo '</option>';
+            };
+            ?>
+        </select>
+    </td>
+    <td class='JB' rowspan=2><a name='b5'>&nbsp;</a></td>
+    <td class='JHS' rowspan=4><a name='b2'>&nbsp;</a></td>
+</tr>
+<tr>
+    <td class='JHS'><a name='b12'>&nbsp;</a></td>
+</tr>
+<tr>
+    <td class='JB'>
+        <select class="form-control sel" id="sel4" onchange="checkDuplicate()">
+            <option value="player4">Joueur 4</option>
+            <?php
+            for ($i=0;$i<count($item);$i++){
+            echo"<option value=' $i '>";
+                echo $item[$i]->nom;
+            echo '</option>';
+            };
+            ?>
+        </select>
+    </td>
+    <td class='JBS' rowspan=2><a name='b5'>&nbsp;</a></td>
+</tr>
+<tr>
+    <td class='JBS'><a name='b11'>&nbsp;</a></td>
+</tr>
+<tr>
+    <td class='JH'>
+        <select class="form-control sel" id="sel5" onchange="checkDuplicate()">
+            <option value="player5">Joueur 5</option>
+            <?php
+            for ($i=0;$i<count($item);$i++){
+            echo"<option value=' $i '>";
+                echo $item[$i]->nom;
+            echo '</option>';
+            };
+            ?>
+        </select>
+    </td>
+    <td class='JH' rowspan=2><a name='b4'>&nbsp;</a></td>
+    <td class='JB' rowspan=4><a name='b1'>&nbsp;</a></td>
+    <td class='JBS' rowspan=8><a name='b0'>&nbsp;</a></td>
+</tr>
+<tr>
+    <td class='JHS'><a name='b10'>&nbsp;</a></td>
+</tr>
+<td class='JB'>
+    <select class="form-control sel" id="sel6" onchange="checkDuplicate()">
+        <option value="player6">Joueur 6</option>
+        <?php
+        for ($i=0;$i<count($item);$i++){
+        echo"<option value=' $i '>";
+            echo $item[$i]->nom;
+        echo '</option>';
+        };
+        ?>
+    </select>
+</td>
+<td class='JHS' rowspan=2><a name='b4'>&nbsp;</a></td>
+</tr>
+<tr>
+<td class='JBS'><a name='b9'>&nbsp;</a></td>
+</tr>
+<tr>
+<td class='JH'>
+    <select class="form-control sel" id="sel7" onchange="checkDuplicate()">
+        <option value="player7">Joueur 7</option>
+        <?php
+        for ($i=0;$i<count($item);$i++){
+        echo"<option value=' $i '>";
+            echo $item[$i]->nom;
+        echo '</option>';
+        };
+        ?>
+    </select>
+</td>
+<td class='JB' rowspan=2><a name='b3'>&nbsp;</a></td>
+<td class='JBS' rowspan=4><a name='b1'>&nbsp;</a></td>
+</tr>
+<tr>
+<td class='JHS'><a name='b8'>&nbsp;</a></td>
+</tr>
+<tr>
+<td class='JB'>
+    <select class="form-control sel" id="sel8" onchange="checkDuplicate()">
+        <option value="player8">Joueur 8</option>
+        <?php
+        for ($i=0;$i<count($item);$i++){
+        echo"<option value=' $i '>";
+            echo $item[$i]->nom;
+        echo '</option>';
+        };
+        ?>
+    </select>
+</td>
+<td class='JBS' rowspan=2><a name='b3'>&nbsp;</a></td>
+</tr>
+<tr>
+<td class='JBS'><a name='b7'>&nbsp;</a></td>
+</tr>
 </table>
 <button type="button" id="validerbtn" class="btn btn-success" onclick="disableInputs()">Valider</button>
-
-
-
-
 <?php   require "$racine/vue/footer.php"; ?>
--- vue/header.php
+++ vue/header.php
@@ -49,26 +49,22 @@
                         <li>
                             <a href="<?php print("$nom_projet/index.php?page=_admin_partie");?>">Admin Partie</a>
                         </li>
-                        <li>
-                            <a href="#">login</a>
+                    </ul>
+                    <ul class="nav pull-right">
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign In <strong class="caret"></strong></a>
+                            <div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
+                                <form method="post" action="login" accept-charset="UTF-8">
+                                    <input style="margin-bottom: 15px;" type="text" placeholder="Username" id="username" name="username">
+                                    <input style="margin-bottom: 15px;" type="password" placeholder="Password" id="password" name="password">
+                                    <input style="float: left; margin-right: 10px;" type="checkbox" name="remember-me" id="remember-me" value="1">
+                                    <label class="string optional" for="user_remember_me"> Remember me</label>
+                                    <input class="btn btn-primary btn-block" type="submit" id="sign-in" value="Sign In">
+                                    <br>
+                                </form>
+                            </div>
                         </li>
                     </ul>
-    <ul class="nav pull-right">
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign In <strong class="caret"></strong></a>
-                        <div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
-                            <form method="post" action="login" accept-charset="UTF-8">
-                                <input style="margin-bottom: 15px;" type="text" placeholder="Username" id="username" name="username">
-                                <input style="margin-bottom: 15px;" type="password" placeholder="Password" id="password" name="password">
-                                <input style="float: left; margin-right: 10px;" type="checkbox" name="remember-me" id="remember-me" value="1">
-                                <label class="string optional" for="user_remember_me"> Remember me</label>
-                                <input class="btn btn-primary btn-block" type="submit" id="sign-in" value="Sign In">
-                                <br>
-
-                            </form>
-                        </div>
-                    </li>
-                </ul>
                     
                 </div>
                 <!-- /.navbar-collapse -->
@@ -78,6 +74,6 @@
         <!-- Full Width Image Header with Logo -->
         <!-- Image backgrounds are set within the full-width-pics.css file. -->
         <header class="image-bg-fluid-height">
-            <img class="img-responsive img-center imgHeader" src="http://www.regnumhotels.com/i/content/444_1_banner.jpg" alt="">
+            <img class="img-responsive img-center imgHeader" src="tenis.jpg" alt="">
         </header>
         <!-- Content Section -->
